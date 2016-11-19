ed306ade028a6039cce22616302b71123f4cf00f
Ivar Trygve Jarlsby <ivartj@ivartj.org>
2016-11-19 23:12:39 +0100

    Made a library for counting number of current users from Irssi logs.

---
+++ irclog/irclog.go
@@ -0,0 +1,68 @@
+package irclog
+
+import (
+	"bufio"
+	"fmt"
+	"regexp"
+	"io"
+	"strconv"
+)
+
+var (
+	// 22:11 -!- Irssi: #example: Total of 113 nicks [6 ops, 0 halfops, 0 voices, 107 normal]
+	regexTotalNick = regexp.MustCompile(`^[0-9]{2}:[0-9]{2} -!- Irssi: #.+?: Total of ([0-9]+) nicks`)
+
+	// 22:25 -!- FooNick [~BarUser@example-host] has joined #example
+	regexJoined = regexp.MustCompile(`^[0-9]{2}:[0-9]{2} -!- .+? \[~.+?@.+?\] has joined`)
+
+	// 22:39 -!- FooNick [~BarUser@example-host] has quit [Ping timeout: 246 seconds]
+	regexQuit   = regexp.MustCompile(`^[0-9]{2}:[0-9]{2} -!- .+? \[~.+?@.+?\] has quit`)
+
+	// 18:16 -!- FooNick [~BarUser@example-host] has left #example [Leave message]
+	regexLeft   = regexp.MustCompile(`^[0-9]{2}:[0-9]{2} -!- .+? \[~.+?@.+?\] has left`)
+)
+
+func NumUsers(log io.Reader) (int, error) {
+
+	scan := bufio.NewScanner(log)
+	numusers := 0
+
+	for scan.Scan() {
+
+		line := scan.Text()
+
+		switch {
+
+		case regexTotalNick.MatchString(line):
+
+			submatches := regexTotalNick.FindStringSubmatch(line)
+			if len(submatches) != 2 {
+				return 0, fmt.Errorf("Failed to capture by regex the number of total nicks from the line '%s'", line)
+			}
+
+			var err error
+			numusers, err = strconv.Atoi(submatches[1])
+			if err != nil {
+				return 0, fmt.Errorf("Failed to parse '%s' as number of total users", submatches[1])
+			}
+
+		case regexJoined.MatchString(line):
+			numusers++
+
+		case regexQuit.MatchString(line):
+			numusers--
+
+		case regexLeft.MatchString(line):
+			numusers--
+
+		}
+
+	}
+
+	if scan.Err() != nil {
+		return 0, fmt.Errorf("Error on scanning line: %s", scan.Err().Error())
+	}
+
+	return numusers, nil
+}
+
