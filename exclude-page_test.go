3d27ebfe45f030eed0fbae0dd474dbdc5fc45c54
Ivar Trygve Jarlsby <ivartj@ivartj.org>
2016-11-21 20:18:18 +0100

    Added some missing test-related source files.

---
+++ exclude-page_test.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"testing"
+	"net/http"
+	"net/url"
+)
+
+func TestExcludePage(t *testing.T) {
+
+	ctx := mainNewContext(testConf)
+
+	sessionCookie, err := testLogin(ctx)
+	if err != nil {
+		t.Fatalf("Failed to log in: %s.\n", err.Error())
+	}
+
+	rw := testNewResponseWriter()
+	req, err := http.NewRequest("POST", "/exclude", nil)
+	if err != nil {
+		t.Fatalf("Failed to create test request: %s.\n", err.Error())
+	}
+
+	req.AddCookie(sessionCookie)
+	req.Form = url.Values(map[string][]string{
+		"name" : []string{ "#test" },
+		"network" : []string{ "irc.example.com" },
+		"exclude-reason" : []string{ "Too much trout-slapping." },
+	})
+	ctx.site.ServeHTTP(rw, req)
+
+	resp, err := rw.GetResponse(req)
+	if err != nil {
+		t.Fatalf("Failed to parse response: %s.\n", err.Error())
+	}
+
+	if resp.StatusCode != 200 {
+		t.Fatalf("Status code when displaying exclusions not 200, but %d.\n", resp.StatusCode)
+	}
+
+}
+
---
+++ web/test-response-writer.go
@@ -0,0 +1,58 @@
+package web
+
+import (
+	"net/http"
+	"bytes"
+	"fmt"
+)
+
+type TestResponseWriter struct{
+	headerWritten bool
+	header http.Header
+	buf *bytes.Buffer
+}
+
+func NewTestResponseWriter() *TestResponseWriter {
+	return &TestResponseWriter{
+		headerWritten: false,
+		header: http.Header(map[string][]string{}),
+		buf: bytes.NewBuffer([]byte{}),
+	}
+}
+
+func (w *TestResponseWriter) Header() http.Header {
+	return w.header
+}
+
+func (w *TestResponseWriter) Write(bs []byte) (int, error) {
+	err := w.writeHeader(http.StatusOK)
+	if err != nil {
+		return 0, nil
+	}
+	return w.buf.Write(bs)
+}
+
+func (w *TestResponseWriter) WriteHeader(code int) {
+	w.writeHeader(code)
+}
+
+func (w *TestResponseWriter) writeHeader(code int) error {
+	if w.headerWritten {
+		return nil
+	}
+	w.headerWritten = true
+	_, err := fmt.Fprintf(w.buf, "HTTP/1.1 %d %s\r\n", code, http.StatusText(code))
+	if err != nil {
+		return err
+	}
+	err = w.header.Write(w.buf)
+	if err != nil {
+		return err
+	}
+	_, err = w.buf.Write([]byte("\r\n"))
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
